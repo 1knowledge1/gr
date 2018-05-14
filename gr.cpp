5812d7c9a7b86abdeeb50c28ac6629ae395ee46a
Yaroslav <32066631+1knowledge1@users.noreply.github.com>
2018-05-14 15:49:56 +0300

    Update gr.cpp

--- gr.cpp
+++ gr.cpp
@@ -1,106 +1,69 @@
 #include "stdafx.h"
+#include <vector>
 #include <iostream>
 #include <sstream>
 #include <fstream>
+
 class Graph
 {
 private:
-	struct vertex
+	bool ** matr;
+	unsigned int num_vertices;
+
+
+	void DFS(unsigned int v, std::vector<bool> & visited, std::ostream & stream)
 	{
-		vertex **next;
-		int val;
-		unsigned int num_edges;
-		int for_dfs;
-		vertex()
-		{
-			next = nullptr;
-			val = 0;
-			num_edges = 0;
-			for_dfs = 0;
-		}
-		~vertex()
-		{
-			//std::cout << val << " destr begine\n";
-			if (next)
-			{
-				//for (unsigned int i = 0; i < num_edges; i++)
-				//{
-				//	std::cout << i << " - edge\n";
-				//	delete next[i];
-				//}
-				delete[] next;
+		visited[v] = true;
+		stream << v << " ";
+		for (int i = 0; i < num_vertices; ++i) {
+			if (matr[v][i] == 1 && !visited[i]) {
+				DFS(i, visited, stream);
 			}
-			//std::cout<< " destr end\n";
-		}
-
-
-		//void dfs_v(vertex * v)
-		//{
-		//	if (dfs == 1)  // Если мы здесь уже были, то тут больше делать нечего
-		//	{
-		//		std::cout << val << " exit, cause dfs=1\n";
-		//		return;
-		//	}
-		//	dfs = 1; // Помечаем, что мы здесь были
-		//	if (next != nullptr)
-		//	{
-		//		for (int i = 0; i < num_edges; ++i)
-		//		{
-		//			std::cout << val << " call DFS for V[" << i << "]\n";
-		//			dfs_v(next[i]);
-		//		}
-		//	}
-		//	else
-		//	{
-		//		return;
-		//	}
-		//}
-	};
-private:
-	int num_vertices;
-	vertex *V;
-
-
-	int nodeindex(vertex *versh) {
-		for (int i = 0; i < num_vertices; i++) {
-			if (&V[i] == versh) {
-				return i + 1;
-			}
-		}
-		return -1;
-	}
-	void dfs(vertex *versh, int *vector, int &m) {
-		if (versh->for_dfs == 1) {
-			return;
-		}
-		versh->for_dfs = 1;
-		vector[m] = nodeindex(versh);
-		m += 1;
-		for (int i = 0; i < versh->num_edges; i++) {
-			dfs(versh->next[i], vector, m);
 		}
 	}
 
 
-	
 public:
 	Graph()
 	{
-		V = nullptr;
+		matr = nullptr;
 		num_vertices = 0;
 	}
 	~Graph()
 	{
-		if (V) delete[] V;
+		if (matr)
+		{
+			for (unsigned int i = 0; i < num_vertices; i++)
+			{
+				delete[] matr[i];
+			}
+			delete[] matr;
+		}
 	}
+
+
+	void DFS(unsigned int v, std::ostream & stream)
+	{
+		if (v < num_vertices) {
+			std::vector<bool> visited(num_vertices);
+			DFS(v, visited, stream);
+		}
+		else {
+			std::ostringstream sstream;
+			sstream << "graph consist " << num_vertices << " vertecies " << v;
+			throw std::out_of_range(sstream.str());
+		}
+	}
+
+
 	bool read(std::istream &stream)
 	{
 		bool success = true;
 		if (stream >> num_vertices)
 		{
-			int ** elements = new int *[num_vertices];
+			bool **elements = new bool *[num_vertices];
 			for (unsigned int i = 0; success && i < num_vertices; ++i) {
-				elements[i] = new int[num_vertices];
+				elements[i] = new bool[num_vertices];
 				for (unsigned int j = 0; j < num_vertices; ++j) {
 					if (!(stream >> elements[i][j])) {
 						success = false;
@@ -114,26 +77,13 @@ public:
 			}
 			if (success)
 			{
-				V = new vertex[num_vertices];
+				matr = new bool *[num_vertices];
 				for (unsigned int i = 0; i < num_vertices; i++)
 				{
-					V[i].val = i + 1;
+					matr[i] = new bool[num_vertices];
 					for (unsigned int j = 0; j < num_vertices; j++)
 					{
-						V[i].num_edges += elements[i][j];
-					}
-					if (V[i].num_edges != 0)
-					{
-						V[i].next = new vertex*[V[i].num_edges];
-						int counter = 0;
-						for (unsigned int j = 0; j < num_vertices; j++)
-						{
-							if (elements[i][j] == 1)
-							{
-								V[i].next[counter] = &V[j];
-								counter++;
-							}
-						}
+						matr[i][j]= elements[i][j];
 					}
 				}
 				for (unsigned int i = 0; i < num_vertices; ++i)
@@ -153,38 +103,14 @@ public:
 		}
 		return success;
 	}
-	/////////////
-	int* dfs() {
-		int*vector = new int[num_vertices];
-		for (int i = 0; i < num_vertices; i++) {
-			vector[i] = -1;
-		}
-		int m = 0;
-		for (int i = 0; i < num_vertices; i++) {
-			if (V[i].for_dfs == 0) {
-				dfs(&V[i], vector, m);
-			}
-		}
-		return vector;
-	}
-	void printdfs(std::ostream & stream) {
-		int *vector = dfs();
-		for (int i = 0; i < num_vertices&&vector[i] != -1; i++) {
-			stream << vector[i] << " ";
-		}
-		stream << '\n';
-	}
-	///////////////////
 };
 int main() {
 	std::ifstream fin;
-	fin.open("E:\\projects\\ConsoleApplication2\\gr.txt");
+	fin.open("D:\\Visual Studio Projects\\gr.txt");
 
 	Graph gr;
-	gr.read(fin);
-	gr.printdfs(std::cout);
-	//for (int i = 0; i< 4; i++)
-	//	std::cout << gr.V[i].val << " - " << gr.V[i].num_edges << '\n';
+	if(gr.read(fin)) gr.DFS(2, std::cout);
+	else std::cout << "error\n";
 
 	return 0;
 }
